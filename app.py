7b7ea4d5f61eaf76c79e49000467111966fffec5
sebastiaouti <sa.medicina@pm.me>
2025-04-03 00:15:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
-from SmartCheck import smartcheck_interface
-from SmartRound import smartround_interface
+from pages.SmartCheck import smartcheck_interface
+from pages.SmartRound import smartround_interface
 
 # Configuração de página
 st.set_page_config(page_title="SmartRound", layout="wide", initial_sidebar_state="expanded")
